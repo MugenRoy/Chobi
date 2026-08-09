96017072898142ee6cb53f5a23b8393440e10777
Shaswata Roy <shaswataroy05@gmail.com>
2026-08-09 23:34:32 +0600

    basic layout foundation

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,7 +1,8 @@
-
 function onWindowClose() {
     Neutralino.app.exit();
 }
 
 Neutralino.init();
 Neutralino.events.on("windowClose", onWindowClose);
+
+const canvas = new fabric.Canvas("photo-area");
