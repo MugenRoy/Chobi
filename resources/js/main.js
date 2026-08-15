885e6acd10f64fb9082e10890094389c515b04c7
saul8521 <ammsabahath2003@gmail.com>
2026-08-16 00:29:57 +0600

    Select Tool Function

--- resources/js/main.js
+++ resources/js/main.js
@@ -6,3 +6,9 @@ Neutralino.init();
 Neutralino.events.on("windowClose", onWindowClose);
 
 const canvas = new fabric.Canvas("photo-area");
+
+const selectionTool = document.getElementById('selection-tool');
+
+selectionTool?.addEventListener('click', () => {
+  setActiveTool('select');
+});
