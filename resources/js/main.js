eb1b244ac3ad5bb044a66c5c322aa94721ae8343
Shaswata Roy <shaswataroy05@gmail.com>
2026-08-15 16:22:13 +0600

    Add test code

--- resources/js/main.js
+++ resources/js/main.js
@@ -6,3 +6,11 @@ Neutralino.init();
 Neutralino.events.on("windowClose", onWindowClose);
 
 const canvas = new fabric.Canvas("photo-area");
+
+function setup() {
+  canvas.setDimensions({
+      width : window.innerWidth * 0.80,
+      height: window.innerHeight * 0.80});
+}
+
+setup();
