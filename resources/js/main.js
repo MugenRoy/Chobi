d046e7b0b8c8597df842e67723fb907f47d553e4
Shaswata Roy <shaswataroy05@gmail.com>
2026-08-27 23:10:03 +0600

    Add delete support

--- resources/js/main.js
+++ resources/js/main.js
@@ -22,3 +22,47 @@ function addText() {
     });
     canvas.add(editableText);
 }
+
+function deleteSelected() {
+    const activeObjects = canvas.getActiveObjects();
+    if (activeObjects.length < 1) {
+        return;
+    }
+    for (const object of activeObjects) {
+        switch (object.type) {
+            case 'textbox': {
+                if (object.isEditing)
+                {
+                    break;
+                }
+                canvas.discardActiveObject();
+                canvas.remove(object);
+                break;
+            }
+            default: {
+                canvas.discardActiveObject();
+                canvas.remove(object);
+                break;
+            }
+        }
+    }
+    canvas.requestRenderAll();
+}
+
+document.addEventListener("keydown", (event) => {
+    const isCtrlOrCmd = event.ctrlKey || event.metaKey;
+    switch (event.code) {
+        case "Delete": {
+            deleteSelected();
+            break;
+        }
+        case isCtrlOrCmd && event.code === "KeyD": {
+            event.preventDefault();
+            console.log("Test");
+            break;
+        }
+        default: {
+            break;
+        }
+    }
+});
